cde2378f4c88f99bacefe45d38295b18a2a9740f
Brandon Neuman <brandon.neuman27@student.tamdistrict.org>
2025-05-22 21:16:00 +0000

    images moved to index

--- images.js
+++ images.js
@@ -1,13 +1,13 @@
-// let playerImages = []
-let playerImage;
+// // let playerImages = []
+// let playerImage;
 
-function preload() {
-    // playerImages.push(loadImages("br.png"));
-    // playerImages.push(loadImages("re.png"));
-    // playerImages.push(loadImages("sp.png"));
-    // playerImages.push(loadImages("ed.png"));
-    // playerImages.push(loadImages("mi.png"));
-    // playerImages.push(loadImages("ma.png"));
+// function preload() {
+//     // playerImages.push(loadImages("br.png"));
+//     // playerImages.push(loadImages("re.png"));
+//     // playerImages.push(loadImages("sp.png"));
+//     // playerImages.push(loadImages("ed.png"));
+//     // playerImages.push(loadImages("mi.png"));
+//     // playerImages.push(loadImages("ma.png"));
 
-    playerImage = loadImage("br.png")
-}
+//     playerImage = loadImage("br.png")
+// }
--- index.js
+++ index.js
@@ -9,8 +9,23 @@ function setup() {
 
 function draw() {
     background(0);
-    //Image(playerImages, 150, 250, 50, 50);
-    Image(playerImage, 150, 250, 50, 50);
+    //Image(playerImages, 225, 450, 50, 50);
+    Image(playerImage, 225, 450, 50, 50);
+}
+
+//Preloader
+// let playerImages = []
+let playerImage;
+
+function preload() {
+    // playerImages.push(loadImages("br.png"));
+    // playerImages.push(loadImages("re.png"));
+    // playerImages.push(loadImages("sp.png"));
+    // playerImages.push(loadImages("ed.png"));
+    // playerImages.push(loadImages("mi.png"));
+    // playerImages.push(loadImages("ma.png"));
+
+    playerImage = loadImage("br.png")
 }
 
 //Other Functions
