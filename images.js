9005de05876de2fb7038f4e7b070f66754d0e006
Brandon Neuman <brandon.neuman27@student.tamdistrict.org>
2025-05-22 00:56:23 +0000

    images

--- images.js
+++ images.js
@@ -1,5 +1,10 @@
-let playerImage;
+let playerImages = []
 
 function preload() {
-    playerImage = loadImage("");
+    playerImages.push(loadImages("br.png"));
+    playerImages.push(loadImages("re.png"));
+    playerImages.push(loadImages("sp.png"));
+    playerImages.push(loadImages("ed.png"));
+    playerImages.push(loadImages("mi.png"));
+    playerImages.push(loadImages("ma.png"));
 }
