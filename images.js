67cb636f69a39bb8442549430974b2c1ccaaa6a1
Brandon Neuman <brandon.neuman27@student.tamdistrict.org>
2025-05-22 21:09:44 +0000

    single player rn

--- images.js
+++ images.js
@@ -1,10 +1,13 @@
-let playerImages = []
+// let playerImages = []
+let playerimage;
 
 function preload() {
-    playerImages.push(loadImages("br.png"));
-    playerImages.push(loadImages("re.png"));
-    playerImages.push(loadImages("sp.png"));
-    playerImages.push(loadImages("ed.png"));
-    playerImages.push(loadImages("mi.png"));
-    playerImages.push(loadImages("ma.png"));
+    // playerImages.push(loadImages("br.png"));
+    // playerImages.push(loadImages("re.png"));
+    // playerImages.push(loadImages("sp.png"));
+    // playerImages.push(loadImages("ed.png"));
+    // playerImages.push(loadImages("mi.png"));
+    // playerImages.push(loadImages("ma.png"));
+
+    playerImage = loadImage("br.png")
 }
