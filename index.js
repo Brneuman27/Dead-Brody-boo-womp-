b9bc3a88ff78611f4c5861c746002bd0a9c165fc
Brandon Neuman <brandon.neuman27@student.tamdistrict.org>
2025-05-22 21:28:05 +0000

    1-28

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 //https://portal.codewithus.com/student/lectures/JavaScript/10    1-15
 
 //Variables
+let player;
 
 //Main Functions
 function setup() {
     createCanvas(500, 500);
+    player = new Player();
 }
 
 function draw() {
     background(0);
-    //Image(playerImages, 225, 450, 50, 50);
-    Image(playerImage, 225, 450, 50, 50);
+    player.draw;
 }
 
-//Preloader
+//Other Functions
+
+//images.js
 // let playerImages = []
 let playerImage;
 
@@ -28,4 +31,16 @@ function preload() {
     playerImage = loadImage("br.png")
 }
 
-//Other Functions
+//player.js
+class Player {
+    constructor() {
+        this.x = 225;
+        this.y = 450;
+        this.w = 50;
+        this.h = 50;
+    }
+    draw() {
+        image(playerImage, this.x, this.y, this.w, this.h);
+        //images(playerImage, this.x, this.y, this.w, this.h);
+    }
+}
