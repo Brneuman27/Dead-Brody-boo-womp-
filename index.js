66b0a85441314955f0364aebae04254128259985
Brandon Neuman <brandon.neuman27@student.tamdistrict.org>
2025-05-22 00:24:03 +0000

    500px walls

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+//https://portal.codewithus.com/student/lectures/JavaScript/10
 
+//Variables
+
+//Main Functions
+function setup() {
+    createCanvas(500, 500);
+}
+
+function draw() {
+    background(0);
+}
+
+//Other Functions
